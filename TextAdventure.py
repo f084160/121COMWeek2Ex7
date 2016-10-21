1fdff0c7588fb814711ea13f917d51cf4f63547f
f084160 <sharma86@uni.coventry.ac.uk>
2016-10-21 10:34:10 +0100

    Update TextAdventure.py

--- TextAdventure.py
+++ TextAdventure.py
@@ -6,14 +6,20 @@ print("\n")
 print("You awake in a dark room.  Do you:")
 print("a) Scream for help.")
 print("b) Press the light switch")
-x = input("Enter a or b: ")
-if x == "a":
-    print("Someone hears your screams...")
-    # Contine adventure Here
-elif x == "b":
+if answer == "b":
+    print("a) Someone hears you switch on the light switch, and the door opens.")
+     print("a man comes in the room with a gun")
+    print("you die")
+elif answer == "a":
+    print("b) A woman comes out, she smiles a sad smile and speaks")
+    shespeaks = input("Do you want to speak to the woman Yes/No")
+if answer == "No":
     print("The light comes on.")
-    print("You do not recognise the room but you have a really bad feeling...")
-    # Contine adventure Here
-else:
-    print("That was not an option.  Game Over")
-
+    print("a man comes in the room with a gun")
+    print("you die")
+elif answer == "Yes":
+        print(" You ask her what is going on")
+        print("she says that shes going to help you escape")
+        print("you say okay, and you both exit the dark room")
+        print("you both come across a hallway, with flickering lights")
+        print ("you slowly
